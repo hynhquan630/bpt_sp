c26654d9fe0654e2d2cf4bb6f34246e370b46ff6
hynhquan630 <hynhquan630@gmail.com>
2024-10-12 16:41:49 +0700

    First

--- data/connect.js
+++ data/connect.js
@@ -1,10 +1,15 @@
 import mysql from 'mysql'
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root123',
-    database: 'bpt',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    multipleStatements: true
 })
 
 db.connect(function (err) {
